f0202e06c2cf5f802ce259f50032600a1241a89f
King <king@kings-Mac-mini.local>
2026-02-28 16:54:27 +0900

    console log

--- bruteforce.js
+++ bruteforce.js
@@ -10,7 +10,7 @@ export function bruteForce(privateKeyBytes, onSuccess) {
 
   importWalletAndCheckBalance(privateKeyWIF).then(result => {
     waitSync(6000);
-    console.log(`${new Date().toISOString()} Generated Private Key : ${Buffer.from(privateKeyBytes).toString('hex')} - ${privateKeyWIF}`);
+    console.log(`${new Date().toISOString()} Generated Private Key By: ${sniperName}, Key: ${Buffer.from(privateKeyBytes).toString('hex')}`);
     console.log(`${new Date().toISOString()} Generated Address: ${result.address}, Tx Count: ${result.chain_stats.tx_count + result.mempool_stats.tx_count}, Balance: ${(result.chain_stats.funded_txo_sum - result.chain_stats.spent_txo_sum + result.mempool_stats.funded_txo_sum - result.mempool_stats.spent_txo_sum) / 1e8} BTC`);
     const balance1 = result.chain_stats.funded_txo_sum - result.chain_stats.spent_txo_sum;
     const balance2 = result.mempool_stats.funded_txo_sum - result.mempool_stats.spent_txo_sum;
--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-import * as bitcoin from 'bitcoinjs-lib';
-import * as ECPairFactory from 'ecpair';
-import * as ecc from 'tiny-secp256k1';
-import axios from 'axios';
-import crypto from 'crypto';
-
-import { initializeApp } from "firebase/app"
-import { getFirestore, collection, addDoc } from "firebase/firestore"
-
 import { writeData } from './firebase.js';
 import { bruteForce } from './bruteforce.js';
 import { generatePrivateKeyForBruteForce } from './wallet.js';
